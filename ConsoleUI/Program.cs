5fb9c93620db0f120cd865695225b7735524f141
EmirSafaYavuz <emirsafayavuz@outlook.com>
2021-02-07 15:05:30 +0300

    07.07.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
